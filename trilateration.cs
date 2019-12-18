509fb4b9a1102714be19eb1f269b23c0b535dd41
Charlie van Rantwijk <charlievanrantwijk@gmail.com>
2019-12-18 10:14:42 +0100

    Add trilateration logic

---
+++ trilateration.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Trilateration
+{
+    public double x, y;
+
+    public Trilateration calculateLocation(double x1, double y1, double r1, double x2, double y2, double r2, double x3, double y3, double r3)
+    {
+        //var coordinates = new { x, y };
+        double A = 2 * x2 - 2 * x1;
+        double B = 2 * y2 - 2 * y1;
+        double C = Math.Pow(r1, 2) - Math.Pow(r2, 2) - Math.Pow(x1, 2) + Math.Pow(x2, 2) - Math.Pow(y1, 2) + Math.Pow(y2, 2);
+        double D = 2 * x3 - 2 * x2;
+        double E = 2 * y3 - 2 * y2;
+        double F = Math.Pow(r2, 2) - Math.Pow(r3, 2) - Math.Pow(x2, 2) + Math.Pow(x3, 2) - Math.Pow(y2, 2) + Math.Pow(y3, 2);
+
+        double x = (C * E - F * B) / (E * A - B * D);
+        double y = (C * D - A * F) / (B * D - A * E);
+
+        Trilateration tri = new Trilateration();
+        tri.x = x;
+        tri.y = y;
+        return tri;
+    }
+}
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Trilateration trilateration = new Trilateration();
+        Trilateration result = trilateration.calculateLocation(-84, 44, 92.1791733528, 98, 64, 171.011695506, 116, -108, 171.39719951);
+        Console.WriteLine("X: {0} Y: {1}", result.x, result.y);
+    }
+}
