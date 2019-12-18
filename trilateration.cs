85d798390808fd99e68c236b1c8e92f4b199b90c
Charlie van Rantwijk <charlievanrantwijk@gmail.com>
2019-12-18 16:29:19 +0100

    Clean up variable declarations

--- trilateration.cs
+++ trilateration.cs
@@ -6,15 +6,18 @@ public class Trilateration
 
     public Trilateration calculateLocation(double x1, double y1, double r1, double x2, double y2, double r2, double x3, double y3, double r3)
     {
-        double A = 2 * x2 - 2 * x1;
-        double B = 2 * y2 - 2 * y1;
-        double C = Math.Pow(r1, 2) - Math.Pow(r2, 2) - Math.Pow(x1, 2) + Math.Pow(x2, 2) - Math.Pow(y1, 2) + Math.Pow(y2, 2);
-        double D = 2 * x3 - 2 * x2;
-        double E = 2 * y3 - 2 * y2;
-        double F = Math.Pow(r2, 2) - Math.Pow(r3, 2) - Math.Pow(x2, 2) + Math.Pow(x3, 2) - Math.Pow(y2, 2) + Math.Pow(y3, 2);
+        double A, B, C, D, E, F;
+        double x, y;
 
-        double x = (C * E - F * B) / (E * A - B * D);
-        double y = (C * D - A * F) / (B * D - A * E);
+        A = 2 * x2 - 2 * x1;
+        B = 2 * y2 - 2 * y1;
+        C = Math.Pow(r1, 2) - Math.Pow(r2, 2) - Math.Pow(x1, 2) + Math.Pow(x2, 2) - Math.Pow(y1, 2) + Math.Pow(y2, 2);
+        D = 2 * x3 - 2 * x2;
+        E = 2 * y3 - 2 * y2;
+        F = Math.Pow(r2, 2) - Math.Pow(r3, 2) - Math.Pow(x2, 2) + Math.Pow(x3, 2) - Math.Pow(y2, 2) + Math.Pow(y3, 2);
+
+        x = (C * E - F * B) / (E * A - B * D);
+        y = (C * D - A * F) / (B * D - A * E);
 
         this.x = x;
         this.y = y;
